bd83cc18c735f448d5aa1d5273417722c76c303a
Alice <34670205+AliceCSY@users.noreply.github.com>
2019-02-27 01:13:57 -0500

    remove currency formatter, change default currency

--- currency-format.js
+++
@@ -1,7 +0,0 @@
-let output = (amount) => {
-	console.log(new Intl.NumberFormat('en-USD', {style: 'currency', currency: 'USD', maximumFractionDigits: 2, minimumFractionDigits: 2}).format(amount));
-};
-
-module.exports = {
-	output
-};
